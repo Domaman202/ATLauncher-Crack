1fee6fbedf8bd6724f6c2b43030b1acd75f64c9a
DomamaN202 <vip.domaman@mail.ru>
2026-05-09 15:09:17 +0700

    Русские не сдаются!

--- src/main/java/ru/DmN/atlcrack2/Bootstrap.java
+++ src/main/java/ru/DmN/atlcrack2/Bootstrap.java
@@ -3,35 +3,52 @@ package ru.DmN.atlcrack2;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Bootstrap {
-    public static void main(String[] args) throws IOException, ClassNotFoundException, InvocationTargetException, IllegalAccessException, NoSuchMethodException, URISyntaxException {
-        Path fileATL = new File("ATLauncher.jar").toPath();
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        Path atlJar = new File("ATLauncher.jar").toPath();
 
-        if (!Files.exists(fileATL)) {
+        if (!Files.exists(atlJar)) {
             System.out.println("Downloading ATLauncher... Please wait!");
-            try (ReadableByteChannel rbc = Channels.newChannel(URI.create("https://atlauncher.com/download/jar").toURL().openStream());
-                 FileOutputStream fos = new FileOutputStream(fileATL.toFile())) {
+            try (ReadableByteChannel rbc = Channels.newChannel(
+                    URI.create("https://atlauncher.com/download/jar").toURL().openStream());
+                 FileOutputStream fos = new FileOutputStream(atlJar.toFile())) {
                 fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
             }
         }
 
-        System.out.println("Starting ATLauncher...");
-        try (URLClassLoader loader = new BootstrapClassLoader(new URL[]{Bootstrap.class.getProtectionDomain().getCodeSource().getLocation().toURI().toURL(), fileATL.normalize().toUri().toURL()}, ClassLoader.getSystemClassLoader())) {
-            Thread.currentThread().setContextClassLoader(loader);
-            Class<?> clazz = loader.loadClass("ru.DmN.atlcrack2.Main");
-            Method method = clazz.getMethod("main", String[].class);
-            method.invoke(null, (Object) args);
+        Path currentJar = new File(Bootstrap.class.getProtectionDomain().getCodeSource().getLocation().toURI()).toPath();
+
+        String classpath = currentJar + File.pathSeparator + atlJar.normalize();
+
+        String javaHome = System.getProperty("java.home");
+        String javaBin = javaHome + File.separator + "bin" + File.separator + "java";
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            javaBin += ".exe";
         }
+
+        List<String> command = new ArrayList<>();
+        command.add(javaBin);
+        command.add("-cp");
+        command.add(classpath);
+        command.add("ru.DmN.atlcrack2.Main");
+        command.addAll(Arrays.asList(args));
+
+        ProcessBuilder pb = new ProcessBuilder(command);
+        pb.inheritIO();
+        System.out.println("Restarting application...");
+        pb.start();
+
+        System.exit(0);
     }
 }
